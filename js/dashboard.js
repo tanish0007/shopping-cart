6eb6dc6df5117aeedfc58feb2f42bc5ca5e33c9c
tanish0007 <jangratanish0007@gmail.com>
2025-06-25 19:10:10 +0000

    all working is done

---
+++ js/dashboard.js
@@ -0,0 +1,292 @@
+const nav = document.querySelector(".nav");
+const pageData = document.querySelector("#page-Data");
+let items = JSON.parse(localStorage.getItem("items")) || [];
+const itemsBox = document.createElement("div");
+itemsBox.classList.add("items-box");
+
+const loggedInUser = JSON.parse(sessionStorage.getItem("loggedInUser"));
+
+if(!loggedInUser) {
+    window.location.href = "index.html";
+} else {
+    showPageData();
+}
+
+
+function showPageData (){
+    const heading = document.createElement("h1");
+    heading.innerText = `Welcome ${loggedInUser.name}`;
+    nav.appendChild(heading);
+
+    const sideNav = document.createElement("div");
+    if(loggedInUser.isAdmin){
+        const span = document.createElement('span');
+        span.innerHTML = "Admin";
+        sideNav.appendChild(span);
+    }
+    const logoutButton = document.createElement("button");
+    logoutButton.type = "button";
+    logoutButton.id = "logoutBtn"
+    logoutButton.innerText = "Logout";
+    sideNav.appendChild(logoutButton);
+    nav.appendChild(sideNav);
+
+    showItems();
+}
+
+function showItems () {
+    if (items.length === 0) {
+        itemsBox.innerText = "No items Present";
+        pageData.appendChild(itemsBox);
+        return;
+    }
+
+    if (loggedInUser.isAdmin) {
+        renderAdminInterface();
+    }
+    else {
+        renderUserView();
+    }
+
+    // render everything on page-Data
+    pageData.appendChild(itemsBox);
+}
+
+function renderAdminInterface(){
+    const upperDiv = document.createElement("div");
+    upperDiv.classList.add("upper-div");
+
+    const inputsBox = document.createElement("div");
+    inputsBox.classList.add("inputs");
+
+    const nameBox = document.createElement("input");
+    nameBox.id = "nameBox";
+    nameBox.type = "text";
+    nameBox.placeholder = "Name of item..";
+
+    const quanBox = document.createElement("input");
+    quanBox.id = "quanBox";
+    quanBox.type = "number";
+    quanBox.placeholder = "Quantity..";
+
+    const priceBox = document.createElement("input");
+    priceBox.id = "priceBox";
+    priceBox.type = "number";
+    priceBox.placeholder = "Price..";
+
+    const descBox = document.createElement("input");
+    descBox.id = "descBox";
+    descBox.type = "text";
+    descBox.placeholder = "Description";
+
+    inputsBox.appendChild(nameBox);
+    inputsBox.appendChild(quanBox);
+    inputsBox.appendChild(priceBox);
+    inputsBox.appendChild(descBox);
+
+    const buttonsBox = document.createElement("div");
+    buttonsBox.classList.add("Buttons");
+
+    const addBtn = document.createElement("button");
+    addBtn.id = "addBtn";
+    addBtn.innerText = "Add Item"; 
+    addBtn.addEventListener("click", () => {
+        const existingUpdNow = document.querySelector("#updNowBtn");
+        if (existingUpdNow) existingUpdNow.remove();
+
+        if (!nameBox.value || !quanBox.value || !priceBox.value || !descBox.value) {
+            alert("Please fill in all fields.");
+            return;
+        }
+
+        const item = {
+            id: Date.now(),
+            name: nameBox.value,
+            quantity: quanBox.value,
+            price: priceBox.value,
+            description: descBox.value
+        };
+
+        items.push(item);
+        localStorage.setItem("items", JSON.stringify(items));
+        lowerDiv.innerHTML = "";
+        items.forEach(i => addToDom(i, lowerDiv, items));
+    });
+
+    buttonsBox.appendChild(addBtn);
+
+    upperDiv.appendChild(inputsBox);
+    upperDiv.appendChild(buttonsBox);
+
+    
+    const lowerDiv = document.createElement("div");
+    lowerDiv.classList.add("lower-div");
+
+    items.forEach(item => {
+        addToDom(item, lowerDiv, items);
+    })
+
+    itemsBox.appendChild(upperDiv)
+    itemsBox.appendChild(lowerDiv);
+}
+
+function addToDom(item, container, itemsArray) {
+    const div = document.createElement("div");
+    div.setAttribute("id", item.id);
+    div.classList.add("item");
+
+    const ul = document.createElement("ul");
+
+    const firstLI = document.createElement("li");
+    firstLI.classList.add("itemName");
+    firstLI.innerText = `Name : ${item.name}`;
+    ul.appendChild(firstLI);
+
+    const secondLI = document.createElement("li");
+    secondLI.classList.add("itemQuantity");
+    secondLI.innerText = `Quantity : ${item.quantity}`;
+    ul.appendChild(secondLI);
+
+    const thirdLI = document.createElement("li");
+    thirdLI.classList.add("itemPrice");
+    thirdLI.innerText = `Price : ${item.price}`;
+    ul.appendChild(thirdLI);
+
+    const fourthLI = document.createElement("li");
+    fourthLI.classList.add("itemDesc");
+    fourthLI.innerText = `Description : ${item.description}`;
+    ul.appendChild(fourthLI);
+
+    div.appendChild(ul);
+
+    const btnBox = document.createElement("div");
+    btnBox.classList.add("button-box");
+
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.addEventListener("click", () => {
+        const confirmDelete = confirm(`Are you sure you want to delete "${item.name}"?`);
+        if (!confirmDelete) return;
+
+        const updatedItems = itemsArray.filter(i => i.id !== item.id);
+        localStorage.setItem("items", JSON.stringify(updatedItems));
+        div.remove();
+    });
+    btnBox.appendChild(delBtn);
+
+    const updBtn = document.createElement("button");
+    updBtn.innerText = "Edit";
+    updBtn.addEventListener("click", () => {
+        const confirmUpdate = confirm(`Are you sure you want to update "${item.name}"?`);
+        if (!confirmUpdate) return;
+
+        // Pre-fill the admin input fields
+        document.querySelector("#nameBox").value = item.name;
+        document.querySelector("#quanBox").value = item.quantity;
+        document.querySelector("#priceBox").value = item.price;
+        document.querySelector("#descBox").value = item.description;
+
+        // Remove existing Update Now button if any
+        const existingUpdNow = document.querySelector("#updNowBtn");
+        if (existingUpdNow) existingUpdNow.remove();
+
+        //  Remove Add Item button
+        const addBtn = document.querySelector("#addBtn");
+        if (addBtn) {
+            addBtn.disabled = true;
+            addBtn.style.cursor = "not-allowed";
+        }
+
+        // Disable Logout button
+        const logoutBtn = document.querySelector("#logoutBtn") 
+
+        if (logoutBtn) {
+            logoutBtn.disabled = true;
+            logoutBtn.style.cursor = "not-allowed";
+        }
+
+        // Creating Update Now Button
+        const updNow = document.createElement("button");
+        updNow.id = "updNowBtn";
+        updNow.innerText = "Update Now";
+        document.querySelector(".Buttons").appendChild(updNow);
+
+        updNow.addEventListener("click", () => {
+            let changed = false;
+            const nameInput = document.querySelector("#nameBox").value;
+            const quanInput = document.querySelector("#quanBox").value;
+            const priceInput = document.querySelector("#priceBox").value;
+            const descInput = document.querySelector("#descBox").value;
+
+            itemsArray = itemsArray.map(elem => {
+                if (elem.id === item.id) {
+                    if (elem.name !== nameInput && nameInput) {
+                        elem.name = nameInput;
+                        changed = true;
+                    }
+                    if (elem.quantity !== quanInput && quanInput) {
+                        elem.quantity = quanInput;
+                        changed = true;
+                    }
+                    if (elem.price !== priceInput && priceInput) {
+                        elem.price = priceInput;
+                        changed = true;
+                    }
+                    if (elem.description !== descInput && descInput) {
+                        elem.description = descInput;
+                        changed = true;
+                    }
+                }
+                return elem;
+            });
+
+            if (changed) {
+                localStorage.setItem("items", JSON.stringify(itemsArray));
+                container.innerHTML = "";
+                itemsArray.forEach(i => addToDom(i, container, itemsArray));
+                updNow.remove();
+            } else {
+                alert("No changes were made");
+            }
+
+
+            if (addBtn) {
+                addBtn.disabled = false;
+                addBtn.style.cursor = "pointer";
+            }
+            if (logoutBtn) {
+                logoutBtn.disabled = false;
+                logoutBtn.style.cursor = "pointer";
+            }
+
+            updNow.remove();
+        });
+    });
+    btnBox.appendChild(updBtn);
+
+    div.appendChild(btnBox);
+    container.appendChild(div);
+}
+
+function renderUserView(){
+    const itemsWrapper = document.createElement("div");
+    itemsWrapper.classList.add("lower-div");
+
+    items.forEach(item => {
+        const div = document.createElement("div");
+        div.classList.add("item");
+
+        const ul = document.createElement("ul");
+
+        ul.innerHTML = `
+            <li>Name: ${item.name}</li>
+            <li>Quantity: ${item.quantity}</li>
+            <li>Price: ${item.price}</li>
+            <li>Description: ${item.description}</li>
+        `;
+
+        div.appendChild(ul);
+        listWrapper.appendChild(div);
+    })
+    itemsBox.appendChild(itemsWrapper);
+}
---
+++ js/script.js
@@ -0,0 +1,173 @@
+const loginTab = document.querySelector("#loginTab");
+const singUpTab = document.querySelector("#signUpTab");
+const tab = document.querySelector(".tab");
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+function clearForm () {
+    tab.innerHTML = '';
+}
+
+function showLoginForm() {
+    clearForm();
+
+    const mailLabel = document.createElement("label");
+    mailLabel.setAttribute("for", "email");
+    mailLabel.innerText = "Email : ";
+    tab.appendChild(mailLabel);
+    const userEmail = document.createElement("input");
+    userEmail.id = "email";
+    userEmail.type = "email";
+    userEmail.placeholder = "user123@gmail.com";
+    mailLabel.appendChild(userEmail);
+
+    const passLabel = document.createElement("label");
+    passLabel.setAttribute("for", "password");
+    passLabel.innerText = "Password : ";
+    tab.appendChild(passLabel);
+    const userPassword = document.createElement("input");
+    userPassword.id = "password";
+    userPassword.type = "password";
+    userPassword.placeholder = "********";
+    passLabel.appendChild(userPassword);
+
+    const errorText = document.createElement("span");
+    errorText.id = "errorText";
+    tab.appendChild(errorText);
+
+    const loginBtn = document.createElement("button");
+    loginBtn.id = "loginBtn";
+    loginBtn.innerText = "Login";
+    loginBtn.type = "button";
+    tab.appendChild(loginBtn);
+
+    loginBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        if(!userEmail.value || !userPassword.value){
+            errorText.innerText = "Please enter all fields";
+            errorText.style.color = "red";
+            return;
+        }
+
+        const existingUser = users.find(user => 
+            user.email === userEmail.value && user.password === userPassword.value
+        );
+
+        if(existingUser){
+            sessionStorage.setItem("loggedInUser", JSON.stringify(existingUser));
+            console.log(existingUser);
+            window.location.href = "dashboard.html";
+        } else {
+            errorText.innerText = "User don't exist";
+            errorText.style.color = "red";
+        }
+
+    })
+}
+
+function showSignUpForm() {
+    clearForm();
+
+    const nameLabel = document.createElement("label");
+    nameLabel.setAttribute("for", "username");
+    nameLabel.innerText = "Name : ";
+    tab.appendChild(nameLabel);
+    const userName = document.createElement("input");
+    userName.id = "username";
+    userName.type = "text";
+    userName.placeholder = "Example";
+    userName.required = true;
+    nameLabel.appendChild(userName);
+
+    const mailLabel = document.createElement("label");
+    mailLabel.setAttribute("for", "email");
+    mailLabel.innerText = "Email : ";
+    tab.appendChild(mailLabel);
+    const userEmail = document.createElement("input");
+    userEmail.id = "email";
+    userEmail.type = "email";
+    userEmail.placeholder = "user123@gmail.com";
+    userEmail.required = true;
+    mailLabel.appendChild(userEmail);
+
+    const passLabel = document.createElement("label");
+    passLabel.setAttribute("for", "password");
+    passLabel.innerText = "Password : ";
+    tab.appendChild(passLabel);
+    const userPassword = document.createElement("input");
+    userPassword.id = "password";
+    userPassword.type = "password";
+    userPassword.placeholder = "********";
+    userPassword.required = true;
+    passLabel.appendChild(userPassword);
+
+    const adminLabel = document.createElement("label");
+    adminLabel.setAttribute('for', 'chkAdmin');
+    adminLabel.innerText = "isAdmin ? ";
+    tab.appendChild(adminLabel);
+    const isAdmin = document.createElement("input");
+    isAdmin.type = "checkbox";
+    isAdmin.id = "chkAdmin";
+    tab.appendChild(isAdmin);
+
+    const errorText = document.createElement("span");
+    errorText.id = "errorText";
+    tab.appendChild(errorText);
+
+    const signUpBtn = document.createElement("button");
+    signUpBtn.id = "signUpBtn";
+    signUpBtn.type = "button";
+    signUpBtn.innerText = "Signup";
+    tab.appendChild(signUpBtn);
+
+    signUpBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        if(!userName.value || !userEmail.value || !userPassword.value){
+            errorText.innerText = "Please enter all fields";
+            errorText.style.color = "red";
+            return;
+        }
+
+        const userExists = users.some(user =>
+            user.name.toLowerCase() === userName.value.toLowerCase() ||
+            user.email.toLowerCase() === userEmail.value.toLowerCase()
+        );
+
+        if (userExists) {
+            errorText.innerText = "User already exists with this username or email.";
+            errorText.style.color = "red";
+            return;
+        }
+
+        let newUser = {
+            id: Date.now(),
+            name: userName.value,
+            email: userEmail.value,
+            password: userPassword.value,
+            isAdmin: isAdmin.checked
+        }
+
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+
+        errorText.innerText = "Signup Successful!!";
+        errorText.style.color = "green";
+        console.log(users);
+    })
+}
+
+loginTab.addEventListener("change", ()=> {
+    if(loginTab.checked) showLoginForm();
+});
+
+singUpTab.addEventListener("change", () => {
+    if(singUpTab.checked) showSignUpForm();
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (loginTab.checked) {
+        showLoginForm();
+    }
+});
